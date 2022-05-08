4e741b1a6f09351897b5e57ee65ad40f3f5ab3d0
Azevedo Rayldo <azevedorayldo@gmail.com>
2022-05-08 12:00:36 -0300

    header logo modificado + menu sm md

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
         <nav className="logo">
           <div className="nav-wrapper cyan darken-4">
             <spam href="#" className="brand-logo "><img width="150" height="150" src="logo expo.jpeg"/></spam>
-            <a href="#" data-target="slide" className="sidenav-trigger show-large" ><i className="material-icons">menu</i></a>
+            <a href="#" data-target="slide" className="sidenav-trigger show-large" ></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down menu">
               <tr>
                 <td >
@@ -28,6 +28,12 @@ const Header = () => {
                     <li className="tdtr"><a href="./add"><IoLogoModelS className="icons"/> Add Veículo <HiDocumentAdd className="icons"/></a></li>
                 </td>
               </tr>
+              </ul>
+
+              <ul id="nav-mobile" className="right hide-on-large-only menu-sm-md">             
+                    <li className="tdtr"><a href="./login"><FaUserCircle className=""/> <RiLoginBoxLine className=""/></a></li>          
+                    <li className="tdtr"><a href="./add"><IoLogoModelS className=""/> <HiDocumentAdd className=""/></a></li>
+               
               </ul>
             </div>
         </nav>
