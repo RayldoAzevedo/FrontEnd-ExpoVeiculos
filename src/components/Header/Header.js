a786136a262a9bda53b1c85bf5e52309477c1ab1
Azevedo Rayldo <azevedorayldo@gmail.com>
2022-05-08 12:05:08 -0300

    header logo modificado + menu sm md

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,34 +31,13 @@ const Header = () => {
               </ul>
 
               <ul id="nav-mobile" className="right hide-on-large-only menu-sm-md">             
-                    <li className="tdtr"><a href="./login"><FaUserCircle className=""/> <RiLoginBoxLine className=""/></a></li>          
-                    <li className="tdtr"><a href="./add"><IoLogoModelS className=""/> <HiDocumentAdd className=""/></a></li>
-               
+                    <li className="tdtr"><a href="./add"><IoLogoModelS className=""/> <HiDocumentAdd className=""/></a></li>                         
+              </ul>
+              <ul id="nav-mobile" className="right hide-on-large-only menu-sm-md">             
+                    <li className="tdtr"><a href="./login"><FaUserCircle className=""/> <RiLoginBoxLine className=""/></a></li>                      
               </ul>
             </div>
         </nav>
-   
-           <ul id="slide" className="sidenav">
-               <li>
-                 <div className="user-view">
-                   <div className="">
-                     <img src="bgCompScience.jpg"/>
-                   </div>
-                   <a href="#user"><img className="circle" src="avatar.png"/></a>
-                   <a href="#name"><span className="white-text name">Fulano de Tal</span></a>
-                   <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-                 </div>
-               </li>
-   
-               <li><a href="#!"><i className="material-icons ">cloud</i><span className=''>Expo-Veículos 2022</span></a></li>
-               <li><a href="./login">Login</a></li>
-              <li><a href="./add">Adicionar Veículo</a></li>
-   
-               <li><div className="divider"></div></li>
-               <li><a className="subheader">Desenvolvedores</a></li>
-               <li><a className="waves-effect" href="#!">Rayldo Azevedo</a></li>
-               <li><a className="waves-effect" href="#!">Gulherme BArros</a></li>
-           </ul>
        </>
     )
    
